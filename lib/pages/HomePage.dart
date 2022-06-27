f49621ea827bcb3f134449f05b5cc206aba2651e
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-06-28 01:40:30 +0600

    fsav2 init

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:badges/badges.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,45 +8,79 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF00A368),
+      //backgroundColor: const Color(0xFF00A368),
+      backgroundColor: Colors.tealAccent,
       body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(right: 20, left: 15, top: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(
-                  Icons.menu,
-                  color: Colors.white,
-                  size: 30,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                      color: const Color(0xFF00A368),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.5),
-                          blurRadius: 2,
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(right: 20, left: 15, top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF00A368),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.5),
+                            blurRadius: 2,
+                          ),
+                        ]),
+                    child: Badge(
+                      badgeColor: Colors.red,
+                      padding: const EdgeInsets.all(6),
+                      badgeContent: const Text(
+                        "3",
+                        style: TextStyle(
+                          color: Colors.white,
                         ),
-                      ]),
-                  child: InkWell(
-                    onTap: () {},
-                    child: const Icon(
-                      CupertinoIcons.cart,
-                      size: 30,
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Icon(
+                          CupertinoIcons.cart,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Welcome to ShopAppV2",
+                    style: TextStyle(
+                      fontSize: 30,
                       color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                )
-              ],
+                  Text(
+                    "Search an Item: ",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+          ],
+        )),
+      ),
     );
   }
 }
