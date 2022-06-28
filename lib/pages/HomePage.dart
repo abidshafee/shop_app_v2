5405b37630174ffadc3d5a385fc149db7a709ae1
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-06-29 01:17:18 +0600

    cmt cngs

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
                   Container(
                     padding: const EdgeInsets.all(8),
                     decoration: BoxDecoration(
-                        color: const Color(0xFF00A368),
+                        color: Colors.tealAccent,
                         borderRadius: BorderRadius.circular(10),
                         boxShadow: [
                           BoxShadow(
--- lib/widgets/CategoriesWidget.dart
+++ lib/widgets/CategoriesWidget.dart
@@ -17,7 +17,7 @@ class CategoriesWidget extends StatelessWidget {
               Text(
                 "Category",
                 style: TextStyle(
-                    fontSize: 25,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Colors.tealAccent),
               ),
@@ -27,6 +27,52 @@ class CategoriesWidget extends StatelessWidget {
                   fontSize: 15,
                   fontWeight: FontWeight.bold,
                   color: Colors.black54,
+                ),
+              )
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //for(int i; i<8; i++)
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Image.asset(
+                        "assets/images/p1.png",
+                        height: 50,
+                        width: 50,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(right: 5),
+                      child: Text(
+                        "Category",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               )
             ],
