d746c5bbfe2d95e45ed82693bd2328909d4f5b7d
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-07-01 18:26:21 +0600

    updtd categories sec

---
+++ lib/models/Category.dart
@@ -0,0 +1,15 @@
+class Category {
+  final String icon, title;
+
+  Category({required this.icon, required this.title});
+}
+
+List<Category> categories = [
+  Category(icon: "assets/images/1.png", title: "Apple"),
+  Category(icon: "assets/images/2.png", title: "Banana"),
+  Category(icon: "assets/images/3.png", title: "Orange"),
+  Category(icon: "assets/images/4.png", title: "Strawberry"),
+  Category(icon: "assets/images/5.png", title: "Lemon"),
+  Category(icon: "assets/images/6.png", title: "Promaganate"),
+  Category(icon: "assets/images/7.png", title: "Grape"),
+];
--- lib/widgets/CategoriesWidget.dart
+++ lib/widgets/CategoriesWidget.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:shop_app_v2/models/Category.dart';
 
 class CategoriesWidget extends StatelessWidget {
   const CategoriesWidget({Key? key}) : super(key: key);
@@ -36,65 +37,68 @@ class CategoriesWidget extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           child: Row(
             children: List.generate(
-              8,
+              categories.length,
               (index) => Padding(
                 padding: const EdgeInsets.only(right: 8),
                 child: CategoryCard(
-                  icon: "assets/images/5.png",
-                  title: "Lemonade",
+                  //icon: "assets/images/5.png",
+                  icon: categories[index].icon,
+                  //title: "Lemonade",
+                  title: categories[index].title,
                   press: () {},
                 ),
               ),
             ),
           ),
+//Previous Layout of Category section
         ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              for (int i = 1; i < 8; i++)
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Image.asset(
-                          "assets/images/$i.png",
-                          height: 50,
-                          width: 50,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(right: 5),
-                        child: Text(
-                          "Category",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-            ],
-          ),
-        )
+        // SingleChildScrollView(
+        //   scrollDirection: Axis.horizontal,
+        //   child: Row(
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     children: [
+        //       for (int i = 1; i < 8; i++)
+        //         Container(
+        //           margin:
+        //               const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        //           height: 50,
+        //           decoration: BoxDecoration(
+        //             borderRadius: BorderRadius.circular(10),
+        //             color: Colors.white,
+        //             boxShadow: [
+        //               BoxShadow(
+        //                 color: Colors.grey.withOpacity(0.5),
+        //                 spreadRadius: 1,
+        //                 blurRadius: 5,
+        //               ),
+        //             ],
+        //           ),
+        //           child: Row(
+        //             children: [
+        //               Padding(
+        //                 padding: const EdgeInsets.all(5),
+        //                 child: Image.asset(
+        //                   "assets/images/$i.png",
+        //                   height: 50,
+        //                   width: 50,
+        //                 ),
+        //               ),
+        //               const Padding(
+        //                 padding: EdgeInsets.only(right: 5),
+        //                 child: Text(
+        //                   "Category",
+        //                   style: TextStyle(
+        //                     fontSize: 16,
+        //                     fontWeight: FontWeight.bold,
+        //                   ),
+        //                 ),
+        //               ),
+        //             ],
+        //           ),
+        //         )
+        //     ],
+        //   ),
+        // )
       ],
     );
   }
